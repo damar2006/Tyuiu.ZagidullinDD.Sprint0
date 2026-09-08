c46d162c49e7eb230ca8ea246e9efe1a35adab31
Damir <zagidullin.damirka@mail.ru>
2026-09-08 23:56:16 +0500

    Task1

--- Tyuiu.ZagidullinDD.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.ZagidullinDD.Sprint0.Task1.V0/Program.cs
@@ -5,8 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            string name = "Игорь";
-            int age = 45;
+            string name = "Дамир";
+            int age = 20;
             Console.WriteLine("Здравствуйте меня зовут" + name + " мне " + age + " лет!");
             Console.ReadKey();
         }
