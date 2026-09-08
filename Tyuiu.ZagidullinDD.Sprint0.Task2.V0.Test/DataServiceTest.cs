5aad03966b759c491d6d94d1aef9f7a06e93b604
Damir <zagidullin.damirka@mail.ru>
2026-09-09 00:46:14 +0500

    Task2

---
+++ Tyuiu.ZagidullinDD.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ZagidullinDD.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.ZagidullinDD.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Игорь";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Игорь", res);
+        }
+    }
+}
