1718a20c9f20acba5383651b05421f1f19b443fa
Damir <zagidullin.damirka@mail.ru>
2026-09-10 19:42:59 +0500

    Task6

---
+++ Tyuiu.ZagidullinDD.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ZagidullinDD.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.ZagidullinDD.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(arraynums));
+            Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(arraynums));
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(arraynums));
+            Console.ReadKey();
+        }
+    }
+}
