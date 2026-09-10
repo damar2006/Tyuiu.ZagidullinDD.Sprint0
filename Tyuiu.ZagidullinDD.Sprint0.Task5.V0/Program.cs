f9041d9df759201e4cf5a737faa072011d0b9967
Damir <zagidullin.damirka@mail.ru>
2026-09-10 19:18:09 +0500

    Task5

---
+++ Tyuiu.ZagidullinDD.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.ZagidullinDD.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.ZagidullinDD.Sprint0.Task5.V0_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + b = " + DataService.Addition(1, 5));
+            Console.WriteLine("A - b = " + DataService.Subtraction(15, 5));
+            Console.WriteLine("A * b = " + DataService.Multiplication(10, 10));
+            Console.WriteLine("A / b = " + DataService.Division(5, 0));
+            Console.ReadKey();
+        }
+    }
+}
